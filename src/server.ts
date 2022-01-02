ca54d81de264bb2ee848a819848120472c096189
Martin Pietschmann <schirkan86@gmx.de>
2022-01-02 20:23:16 +0100

    first checkin

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { Server, Origins } from 'boardgame.io/server';
+import { AzulGame } from './../client/src/games/azul/Game';
+const serve = require('koa-static');
+
+const server = Server({
+  games: [AzulGame],
+  origins: [Origins.LOCALHOST],
+});
+
+const publicDir = __dirname + '/public';
+server.app.use(serve(publicDir));
+server.run(8000, () => console.log('Server started'));
